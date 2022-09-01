a278de286f636a9b3c17ce7e305fdd9df8ae3006
Tyler Morgan <clickjaw@outlook.com>
2022-09-01 14:47:29 -0500

    front book

--- client/src/components/Books.js
+++ client/src/components/Books.js
@@ -1,27 +1,43 @@
-
 import React, { Component } from 'react'
 import axios from 'axios';
-
+import Card from "react-bootstrap/Card"
 
 export default class Books extends Component {
-constructor(props) {
-  super(props)
 
-  this.state = {
-     book: []
-  }
-}
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         books: [],
+      }
+    }
 
-getBooks=async()=>{
-    const API = "http://localhost:3002/books"
-    const res = await axios.get(API)
-    console.log(res.data)
-}
+    getBooks = async() =>{            
+            const API = "http://localhost:3002/books";
+            let res = await axios.get(API);
+            this.setState({
+              bookList: res.data
+            })
+            // console.log(this.description)
+            // this.setState({dueDate: res.dueDate})
+            console.log(this.state.bookList)
+
+    }
+
 
   render() {
-
     return (
-      <div>Books</div>
+      <div>
+        <button onClick={this.getToDo}>To Do</button>
+        {this.state.toDoList.map((obj)=>
+        <Card>
+        title = {obj.title}<br/>
+        description = {obj.description}
+        status = {obj.status}
+        </Card>
+        )}
+
+      </div>
     )
   }
 }
