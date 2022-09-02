8caafe8f8f38f2fc8dd792c22386eb2b7baf61f4
Curtrick Walton <curtrickw@yahoo.com>
2022-09-01 23:29:37 -0500

    finished lab

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { Component } from 'react'
+
+import Books from "./components/Books"
+
+export default class App extends Component {
+  render() {
+    return (
+        <Books/>
+    )
+  }
+}
+
