c0594eb29c358e33a225770f3e7da3d603c05675
Curtrick Walton <curtrickw@yahoo.com>
2022-09-02 10:58:44 -0500

    add componentDidMount

--- client/src/components/Books.js
+++ client/src/components/Books.js
@@ -1,43 +1,66 @@
-import React, { Component } from 'react'
-import axios from 'axios';
-import Card from "react-bootstrap/Card"
+import React, { Component } from "react";
+import axios from "axios";
+import { Card, Carousel } from "react-bootstrap";
 
 export default class Books extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         books: [],
-      }
-    }
+    this.state = {
+      books: [],
+    };
+  }
+  componentDidMount() {
+    this.getBooks();
+  }
 
-    getBooks = async() =>{            
-            const API = "http://localhost:3002/books";
-            let res = await axios.get(API);
-            this.setState({
-              books: res.data
-            })
-            // console.log(this.description)
-            // this.setState({dueDate: res.dueDate})
-            console.log(this.state.books)
-
-    }
-
+  getBooks = async () => {
+    const API = "http://localhost:3002/books";
+    let res = await axios.get(API);
+    this.setState({
+      books: res.data,
+    });
+    // console.log(this.description)
+    // this.setState({dueDate: res.dueDate})
+    console.log(this.state.books);
+  };
 
   render() {
     return (
       <div>
-        <button onClick={this.getBooks}>To Do</button>
-        {this.state.books.map((obj)=>
-        <Card>
-        title = {obj.title}<br/>
-        description = {obj.description}<br/>
-        status = {obj.status}
-        </Card>
-        )}
-
+        <Carousel
+          fade
+          style={{
+            alignItems: "center",
+            marginLeft: "10px",
+            textAlign: "center",
+            width: "450px",
+            height: "150px",
+            color: "white",
+            boxShadow: "5px 5px 5px white",
+          }}
+        >
+          {this.state.books.map((obj) => {
+            return (
+              <Carousel.Item key={obj.id}>
+                <Card
+                  style={{
+                    backgroundColor: "grey",
+                  }}
+                >
+                  <Card.Body>
+                    <Card.Title>{obj.title}</Card.Title>
+                    <Card.Text>
+                      Description: {obj.description} <br />
+                      status: {obj.status}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
       </div>
-    )
+    );
   }
 }
